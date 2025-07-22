c8ad781a236782fb348d8e173a6bbdda7381e83a
TripleSf1 <SssFBusiness3sf1@gmail.com>
2025-07-22 22:31:43 +0545

    update with brackets

--- script3.js
+++ script3.js
@@ -5,22 +5,23 @@ gsap.from("#box3", {
     rotate: 360,
     backgroundColor: "green",
     borderRadius: "50%",
-    repeat: 3,
+    repeat: -1,
     scale: 0.5,
     opacity: 0,
     yoyo: true,
 
-})
+});
+
 gsap.from("#box1", {
     y: 30,
     opacity: 0,
     duration: 4,
     delay: 1,
-    repeat: 3,
+    repeat: -1,
     width: "0",
     yoyo: true,
 
-})
+});
 
 gsap.from("h1", {
     y: 30,
@@ -28,7 +29,15 @@ gsap.from("h1", {
     delay: 1,
     opacity: 0,
     stagger: 1,
-    repeat: 3,
+    repeat: -1,
     yoyo: true,
 
-})
+});
+
+let timeLine = gsap.timeline();
+timeLine.to(".container", {
+    x: 1400,
+    y: 400,
+    duration: 1.5,
+    delay: 1,
+});
